4c80f351ef9604703f48f632a683d820c1c8fa55
BernEugen <berneugen@gmail.com>
2015-06-29 14:00:12 +0300

    delete sort start menu

--- app/src/main/java/com/cristaliza/alimentation/global/ApiManager.java
+++ app/src/main/java/com/cristaliza/alimentation/global/ApiManager.java
@@ -91,15 +91,15 @@ public abstract class ApiManager {
     public static List<Item> getFirstList() {
         List<Item> list = model.getFirstLevel();
 
-        for (int i = 0; i < list.size(); ++i){
-            for(int j = i+1; j < list.size(); ++j){
-                if(list.get(j).getId().trim().compareTo(list.get(i).getId().trim())<0){
-                    Item temp = list.get(j);
-                    list.set(j,list.get(i));
-                    list.set(i,temp);
-                }
-            }
-        }
+//        for (int i = 0; i < list.size(); ++i){
+//            for(int j = i+1; j < list.size(); ++j){
+//                if(list.get(j).getId().trim().compareTo(list.get(i).getId().trim())<0){
+//                    Item temp = list.get(j);
+//                    list.set(j,list.get(i));
+//                    list.set(i,temp);
+//                }
+//            }
+//        }
         return list;
     }
 
